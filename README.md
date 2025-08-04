403ee6241cb7da9cb48dd259ae53c4ec89b565b4
Goodness-Chukwudi <ibechechukwudi@gmail.com>
2025-08-04 15:58:12 +0100

    added prod knex setting

--- README.md
+++ README.md
@@ -181,4 +181,4 @@ ISC
 
 ---
 
-**Author:** Goodness
+**Author:** Chukwudi Ibeche
--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,9 +1,9 @@
-import type { Knex } from 'knex';
-import Env from './common/environment_variables';
+import type { Knex } from "knex";
+import Env from "./common/environment_variables";
 
 const config: { [key: string]: Knex.Config } = {
   development: {
-    client: 'mysql2',
+    client: "mysql2",
     connection: {
       host: Env.DB_HOST,
       user: Env.DB_USER,
@@ -13,10 +13,25 @@ const config: { [key: string]: Knex.Config } = {
     },
     pool: { min: 0, max: 5 },
     migrations: {
-      tableName: 'knex_migrations',
-      extension: 'ts',
-    },
+      tableName: "knex_migrations",
+      extension: "ts"
+    }
   },
+  production: {
+    client: "mysql2",
+    connection: {
+      host: Env.DB_HOST,
+      user: Env.DB_USER,
+      password: Env.DB_PASSWORD,
+      database: Env.DB_NAME,
+      port: Env.DB_PORT
+    },
+    pool: { min: 0, max: 5 },
+    migrations: {
+      tableName: "knex_migrations",
+      extension: "ts"
+    }
+  }
 };
 
 export default config;
