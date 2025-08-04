9effbd2605f0f1113609432d84b82325bfafc226
Goodness-Chukwudi <ibechechukwudi@gmail.com>
2025-08-04 16:19:58 +0100

    Added start script

--- src/index.ts
+++ src/index.ts
@@ -7,23 +7,25 @@ validateEnvironmentVariables();
 
 App.listen(Env.PORT, async () => {
   if (Env.ENVIRONMENT == Environments.DEV) {
-    console.log(`Express is listening on http://localhost:${Env.PORT}${Env.API_PATH}`);
+    console.log(
+      `Express is listening on http://localhost:${Env.PORT}${Env.API_PATH}`
+    );
   }
 });
 
-process.on('unhandledRejection', (reason: string, p: Promise<any>) => {
-  console.error('Unhandled Rejection at:\n', p);
-  console.log("\n")
-  console.error('Reason:\n', reason);
+process.on("unhandledRejection", (reason: string, p: Promise<any>) => {
+  console.error("Unhandled Rejection at:\n", p);
+  console.log("\n");
+  console.error("Reason:\n", reason);
   //Track error with error logger
-  
+
   process.exit(1);
   //Restart with pm2 in production
 });
 
-process.on('uncaughtException', (error: Error) => {
+process.on("uncaughtException", (error: Error) => {
   console.error(`Uncaught exception:`);
-  console.log("\n")
+  console.log("\n");
   console.error(error);
   //Track error with error logger
 
